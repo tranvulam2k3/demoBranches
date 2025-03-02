d7b0d80b5ea67a71d2b80ee673da1fe95c55e717
tranvulam <tranvulam2392k3@gmail.com>
2025-03-03 00:16:15 +0700

    xin chao

---
+++ demoBranches/src/main/java/com/example/demobranches/DemoBranchesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobranches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBranchesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBranchesApplication.class, args);
+    }
+
+}
---
+++ demoBranches/src/test/java/com/example/demobranches/DemoBranchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demobranches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBranchesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

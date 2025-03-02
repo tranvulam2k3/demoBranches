9bb35df430d98cfc171e01c8e1c44fe57e9726fb
tranvulam <tranvulam2392k3@gmail.com>
2025-03-03 01:25:07 +0700

    branches phan trang commit

---
+++ demoBranches/src/main/java/com/example/demobranches/controller/PhanTrangController.java
@@ -0,0 +1,8 @@
+package com.example.demobranches.controller;
+
+public class PhanTrangController {
+    public String PhanTrang(){
+        //đây là nhánh phân trang
+        return "Phan Trang";
+    }
+}

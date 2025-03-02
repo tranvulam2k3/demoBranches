30e81c58b6487d1da5292ccfb4f43f62ea4bc16d
tranvulam <tranvulam2392k3@gmail.com>
2025-03-03 01:29:25 +0700

    branches commit login

--- demoBranches/src/main/java/com/example/demobranches/controller/LoginController.java
+++ demoBranches/src/main/java/com/example/demobranches/controller/LoginController.java
@@ -3,6 +3,6 @@ package com.example.demobranches.controller;
 public class LoginController {
     //Login làm ở đây
     public void LoginPage(){
-
+        System.out.println("hello xin chào mọi người");
     }
 }
